3cab778aaded7da3cfad016dd6461c2823ade40f
Hermes <hermeschigoziri@gmail.com>
2024-11-29 20:47:43 +0100

    build the getAppSeller and updateAppSeller functions

--- src/main/kotlin/program.data/ProgramData.kt
+++ src/main/kotlin/program.data/ProgramData.kt
@@ -1,7 +1,9 @@
+// This file will hold functions for managing the persisted program data - seller/customer data
 package program.data
 
 import kotlinx.serialization.json.Json
 import modes.Seller
+import java.io.File
 
 data class UpdateAppSellerOptions(
     val updateName: Boolean,
@@ -12,10 +14,29 @@ data class UpdateAppSellerOptions(
 val DEFAULT_UPDATE_APP_SELLER_OPTIONS =
     UpdateAppSellerOptions(updateName = true, updateRegisteredProducts = false, updateInventory = false)
 
-// This file will hold functions for managing the persisted program data - seller/customer data
-fun getAppSeller(): Seller? = null
+fun getAppSeller(): Seller? {
+    val sellerFile = File("appdata", "seller.marketplace")
+    if (sellerFile.exists()) {
+        return Json.decodeFromString<Seller>(sellerFile.readText())
+    }
+    sellerFile.createNewFile()
+    return null
+}
 
 fun updateAppSeller(
     seller: Seller, options: UpdateAppSellerOptions = DEFAULT_UPDATE_APP_SELLER_OPTIONS
 ) {
+    /*
+    * Write the seller data to the seller.marketplace file
+    * The data written might depend on the 'options' parameter(This might not be needed,
+    * will depend on whether all backing fields serialize properly to JSON)
+    * */
+    val stringifiedData = Json.encodeToString(Seller.serializer(), seller)
+    try {
+        val sellerFile = File("appdata", "seller.marketplace")
+        sellerFile.createNewFile()
+        sellerFile.writeText(stringifiedData)
+    } catch (e: Exception) {
+        println("Error while attempting to write to seller.marketplace file: ${e.message}")
+    }
 }
