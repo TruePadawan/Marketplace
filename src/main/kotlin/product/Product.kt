374d9ce6aa0b38e0a9923e3bad3f5bb3f9adb22b
Hermes <hermeschigoziri@gmail.com>
2024-12-04 02:15:28 +0100

    define some default values for the members of the Product class

--- src/main/kotlin/product/Product.kt
+++ src/main/kotlin/product/Product.kt
@@ -1,13 +1,14 @@
 package product
 
 import kotlinx.serialization.Serializable
+import java.util.UUID
 
 @Serializable
 data class Product(
-    val productId: String,
+    val productId: String = UUID.randomUUID().toString(),
     var name: String,
     val price: Double,
-    var amount: Int,
-    var isInInventory: Boolean,
+    var amount: Int = 0,
+    var isInInventory: Boolean = false,
     val sellerId: String
 )
